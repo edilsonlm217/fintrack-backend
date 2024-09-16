b86d59ff010b77fb731581f89083888c883f2fa3
Edilson R Lima <edilsonlm217@hotmail.com>
2024-09-16 00:35:13 -0400

    chore: imported MongoModule and CommitmentModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongoModule } from './database/mongo.module';
+import { CommitmentModule } from './modules/commitment/commitment.module';
 
 @Module({
-  imports: [],
+  imports: [MongoModule, CommitmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
