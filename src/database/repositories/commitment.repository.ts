e3fef66a66cf8019f4136e04ec8de1ad65f9fa56
Edilson R Lima <edilsonlm217@hotmail.com>
2024-09-16 00:41:03 -0400

    feat: defined CommitmentRepository logic

---
+++ src/database/repositories/commitment.repository.ts
@@ -0,0 +1,39 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Db, Collection, ObjectId } from 'mongodb';
+import { Commitment } from 'src/common/interfaces/commitment.interface';
+import { CreateCommitmentDto } from 'src/modules/commitment/dto/create-commitment.dto';
+
+@Injectable()
+export class CommitmentRepository {
+  private readonly collection: Collection;
+
+  constructor(
+    @Inject('MONGO_CONNECTION') private readonly db: Db,
+  ) {
+    this.collection = this.db.collection('financial-commitments');
+  }
+
+  async findAll() {
+    return this.collection.find().toArray();
+  }
+
+  async findOne(id: string) {
+    return this.collection.findOne({ _id: new ObjectId(id) });
+  }
+
+  async insertOne(document: Partial<CreateCommitmentDto>): Promise<Commitment> {
+    const result = await this.collection.insertOne(document);
+
+    if (result.acknowledged) {
+      return { _id: result.insertedId.toString(), ...document } as Commitment;
+    }
+  }
+
+  async updateOne(id: string, document: any) {
+    return this.collection.updateOne({ _id: new ObjectId(id) }, { $set: document });
+  }
+
+  async deleteOne(id: string) {
+    return this.collection.deleteOne({ _id: new ObjectId(id) });
+  }
+}
