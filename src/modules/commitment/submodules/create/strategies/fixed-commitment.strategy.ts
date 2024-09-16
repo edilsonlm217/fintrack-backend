75d806db2d0affb53725311c441d88719e489c33
Edilson R Lima <edilsonlm217@hotmail.com>
2024-09-16 00:44:46 -0400

    feat: implemented FixedCommitmentStrategy

---
+++ src/modules/commitment/submodules/create/strategies/fixed-commitment.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { CommitmentStrategy } from '../interfaces/create-strategy.interface';
+import { CreateCommitmentDto } from 'src/modules/commitment/dto/create-commitment.dto';
+import { CommitmentRepository } from 'src/database/repositories/commitment.repository';
+
+@Injectable()
+export class FixedCommitmentStrategy implements CommitmentStrategy {
+  constructor(private readonly commitmentRepository: CommitmentRepository) { }
+
+  async process(createCommitmentDto: CreateCommitmentDto) {
+    return this.commitmentRepository.insertOne({
+      type: createCommitmentDto.type,
+      description: createCommitmentDto.description,
+      amount: createCommitmentDto.amount,
+      due_date: createCommitmentDto.due_date,
+      category: createCommitmentDto.category,
+      subcategory: createCommitmentDto.subcategory
+    });
+  }
+}
