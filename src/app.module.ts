3ad7a715e810bbb7e8c9ff06e06170e01cf6848b
Edilson R Lima <edilsonlm217@hotmail.com>
2024-09-17 08:55:09 -0400

    feat: added account and transaction modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { MongoModule } from './database/mongo.module';
 import { CommitmentModule } from './modules/commitment/commitment.module';
+import { AccountModule } from './modules/account/account.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [MongoModule, CommitmentModule],
+  imports: [MongoModule, AccountModule, TransactionModule, CommitmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
