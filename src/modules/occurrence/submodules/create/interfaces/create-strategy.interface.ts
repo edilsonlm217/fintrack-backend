7d0245da32687c8ec59d12bd3aabf6876fed9b80
Edilson R Lima <edilsonlm217@hotmail.com>
2024-10-05 21:29:23 -0400

    refactor: refactored for adding documentation

--- src/modules/occurrence/submodules/create/interfaces/create-strategy.interface.ts
+++ src/modules/occurrence/submodules/create/interfaces/create-strategy.interface.ts
@@ -1,6 +1,14 @@
 import { Commitment } from "src/common/interfaces/commitment.interface";
 import { Occurrence } from "src/common/interfaces/occurrence.interface";
 
+/**
+ * Interface for strategies that process commitments to generate occurrences.
+ */
 export interface OccurrenceStrategy {
+  /**
+   * Processes a commitment and generates a list of occurrences.
+   * @param commitment - The commitment to process, containing relevant details.
+   * @returns A promise that resolves to an array of occurrences generated from the commitment.
+   */
   process(commitment: Commitment): Promise<Occurrence[]>;
 }
