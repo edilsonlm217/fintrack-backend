43175b12aadd200cd1494367bc6cfa8bfa256d7e
Edilson R Lima <edilsonlm217@hotmail.com>
2024-10-19 13:30:25 -0400

    refactor: refactored CreateService concern for better application design

--- src/modules/occurrence/submodules/create/create.service.ts
+++ src/modules/occurrence/submodules/create/create.service.ts
@@ -1,34 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 import { Commitment } from 'src/common/interfaces/commitment.interface';
-import { CommitmentType } from 'src/common/enums/commitment-type.enum';
 
-import { OccurrenceStrategy } from './interfaces/create-strategy.interface';
-import { InstallmentOccurrenceStrategy } from './strategies/installment-occurrence.strategy';
-import { RecurringOccurrenceStrategy } from './strategies/recurring-occurrence.strategy';
-import { OneTimeOccurrenceStrategy } from './strategies/one-time-occurrence.strategy';
+import { OccurrencePersistenceService } from './submodules/occurrence-persistence/occurrence-persistence.service';
+import { OccurrenceGenerationService } from './submodules/occurrence-generation/occurrence-generation.service';
 
 @Injectable()
 export class CreateService {
-  private strategies: Record<CommitmentType, OccurrenceStrategy>;
-
   constructor(
-    private readonly recurringStrategy: RecurringOccurrenceStrategy,
-    private readonly installmentStrategy: InstallmentOccurrenceStrategy,
-    private readonly oneTimeStrategy: OneTimeOccurrenceStrategy,
-  ) {
-    this.strategies = {
-      [CommitmentType.RECURRING]: this.recurringStrategy,
-      [CommitmentType.INSTALLMENT]: this.installmentStrategy,
-      [CommitmentType.ONE_TIME]: this.oneTimeStrategy,
-    };
-  }
+    private readonly occurrenceGenerationService: OccurrenceGenerationService,
+    private readonly occurrencePersistenceService: OccurrencePersistenceService,
+  ) { }
 
   async process(commitment: Commitment) {
-    const strategy = this.strategies[commitment.type];
-    if (!strategy) {
-      throw new Error(`Strategy not found for type ${commitment.type}`);
-    }
-    return strategy.process(commitment);
+    const occurrencesToBePersisted = await this.occurrenceGenerationService.process(commitment);
+    const occurrences = await this.occurrencePersistenceService.saveOccurrences(occurrencesToBePersisted);
+    return occurrences;
   }
 }
