a6fb1e2cb286dd58af335bde0b6442b4c5f8d29b
Edilson R Lima <edilsonlm217@hotmail.com>
2024-10-19 13:49:34 -0400

    feat: added CreateModule to abstract commitment creation logic

--- src/modules/commitment/submodules/create/create.module.ts
+++ src/modules/commitment/submodules/create/create.module.ts
@@ -1,20 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CreateService } from './create.service';
-import { FixedCommitmentStrategy } from './strategies/fixed-commitment.strategy';
+import { RecurringCommitmentStrategy } from './strategies/recurring.commitment.strategy';
 import { InstallmentCommitmentStrategy } from './strategies/installment-commitment.strategy';
 import { OneTimeCommitmentStrategy } from './strategies/one-time-commitment.strategy';
-import { PlannedCommitmentStrategy } from './strategies/planned-commitment.strategy';
-import { RecurringCommitmentStrategy } from './strategies/recurring.commitment.strategy';
 
 @Module({
   imports: [],
   controllers: [],
   providers: [
     CreateService,
-    FixedCommitmentStrategy,
     InstallmentCommitmentStrategy,
     OneTimeCommitmentStrategy,
-    PlannedCommitmentStrategy,
     RecurringCommitmentStrategy
   ],
   exports: [CreateService]
