dd0caabde3360049504ca07aa06fc5d127ef28c7
Edilson R Lima <edilsonlm217@hotmail.com>
2024-12-06 18:47:54 -0400

    refactor: added user_id to CreateOccurrenceDto

--- src/common/factories/create-occurrence-dto.factory.ts
+++ src/common/factories/create-occurrence-dto.factory.ts
@@ -7,6 +7,7 @@ export class CreateOccurrenceDtoFactory {
   static createOccurrence(commitment: Commitment, occurrenceDate: string, amount: number): CreateOccurrenceDto {
     return {
       commitment_id: commitment._id,
+      user_id: commitment.user_id,
       due_date: occurrenceDate,
       amount,
       status: OccurrenceStatus.PENDING,
