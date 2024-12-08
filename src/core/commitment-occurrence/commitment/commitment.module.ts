0cb7a7ae44755e627f5250fa608b91376d20f978
Edilson R Lima <edilsonlm217@hotmail.com>
2024-12-08 19:36:21 -0400

    refactor: removed unnecessary controller

--- src/core/commitment-occurrence/commitment/commitment.module.ts
+++ src/core/commitment-occurrence/commitment/commitment.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CommitmentController } from '../../../modules/commitment/commitment.controller';
 import { CommitmentService } from './commitment.service';
 import { CreateModule } from './create/create.module';
 import { FindModule } from './find/find.module';
 
 @Module({
   imports: [CreateModule, FindModule],
-  controllers: [CommitmentController],
+  controllers: [],
   providers: [CommitmentService],
   exports: [CommitmentService]
 })
