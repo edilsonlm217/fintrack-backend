033ea122a4c7c1a7e0dd2a76bab8cd5a38224fc5
Edilson R Lima <edilson.lima@meta.com.br>
2025-02-02 14:53:59 +0000

    feat: add method signatures to CommitmentRepository and OccurrenceRepository

--- src/database/neo4j/repositories/commitment.repository.ts
+++ src/database/neo4j/repositories/commitment.repository.ts
@@ -1,6 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Driver } from 'neo4j-driver';
+
+import { CreateCommitmentDto } from 'src/common/dto/create-commitment.dto';
+import { Commitment } from 'src/common/interfaces/commitment.interface';
 
 @Injectable()
 export class CommitmentRepository {
-  constructor() { }
+  constructor(
+    @Inject('NEO4J_CONNECTION')
+    private readonly neo4jDriver: Driver,
+  ) { }
+
+  async insertOne(commitment: Partial<CreateCommitmentDto>) {
+    const session = this.neo4jDriver.session();
+    const result = await session.executeWrite(tx => tx.run(`
+      CREATE (c:Commitment)
+      SET c = $props
+      RETURN c {
+        .*,
+        _id: elementId(c)
+      }
+    `, {
+      props: commitment
+    }));
+
+    return result.records[0].get('c') as Commitment;
+  }
+
+  findByIds(commitmentIds: string[]): Promise<Commitment[]> {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/database/neo4j/repositories/occurrence.repository.ts
+++ src/database/neo4j/repositories/occurrence.repository.ts
@@ -1,7 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Driver } from 'neo4j-driver';
+import { CreateOccurrenceDto } from 'src/common/dto/create-occurrence.dto';
+import { Occurrence } from 'src/common/interfaces/occurrence.interface';
 
 @Injectable()
 export class OccurrenceRepository {
   constructor(
+    @Inject('NEO4J_CONNECTION')
+    private readonly neo4jDriver: Driver,
   ) { }
+
+  async insertMany(occurrences: CreateOccurrenceDto[]): Promise<Occurrence[]> {
+    const session = this.neo4jDriver.session();
+
+    try {
+      const result = await session.executeWrite(async (tx) => {
+        const query = `
+          UNWIND $occurrences AS occurrence
+          MATCH (c:Commitment)
+          WHERE elementId(c) = occurrence.commitment_id
+          CREATE (o:Occurrence)
+          SET o = occurrence
+          WITH o, c
+          CREATE (o)-[:INSTANCE_OF]->(c)
+          RETURN o {
+            .*,
+            _id: elementId(o)
+          }
+        `;
+
+        const response = await tx.run(query, { occurrences });
+        return response.records.map(record => record.get('o'));
+      });
+
+      return result;
+    } finally {
+      await session.close();
+    }
+  }
+
+  findAllByDateRange(arg0: { userId: string; startDate: string; endDate: string; }): Promise<Occurrence[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  findAllByMonth(arg0: { userId: string; month: number; year: number; }): Promise<Occurrence[]> {
+    throw new Error('Method not implemented.');
+  }
 }
