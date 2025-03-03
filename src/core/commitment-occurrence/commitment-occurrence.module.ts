927132039b5813cebe6ba4178ff079c7f8ba16cc
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-03 19:18:21 -0400

    refactor: removed unnecessary logic

--- src/core/commitment-occurrence/commitment-occurrence.module.ts
+++ src/core/commitment-occurrence/commitment-occurrence.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { CommitmentModule } from './commitment/commitment.module';
 import { OccurrenceModule } from './occurrence/occurrence.module';
 import { CommitmentOccurrenceService } from './commitment-occurrence.service';
-import { CommitmentMapperService } from './commitment-mapper/commitment-mapper.service';
 
 @Module({
   imports: [CommitmentModule, OccurrenceModule],
   controllers: [],
   providers: [
     CommitmentOccurrenceService,
-    CommitmentMapperService,
   ],
   exports: [CommitmentOccurrenceService]
 })
