476ef1a211d2c378bedf32cabdde7cfa0de57a8b
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-03 19:21:40 -0400

    feat: added insertMany method to OccurrenceRepository

---
+++ src/database/neo4j/repositories/occurrence.repository.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Inject } from '@nestjs/common';
+import { Driver } from 'neo4j-driver';
+import { CreateOccurrenceDto } from 'src/common/dto/create-occurrence.dto';
+import { Occurrence } from 'src/common/interfaces/occurrence.interface';
+
+@Injectable()
+export class OccurrenceRepository {
+  constructor(@Inject('NEO4J_DRIVER') private readonly neo4jDriver: Driver) { }
+
+  async insertMany(createOccurrenceDto: Partial<CreateOccurrenceDto>[]): Promise<Occurrence[]> {
+    const session = this.neo4jDriver.session();
+
+    try {
+      const query = `
+        UNWIND $createOccurrenceDto AS doc
+        MATCH (c:Commitment { _id: doc.commitment_id })
+        CREATE (o:Occurrence)
+        SET o += doc
+        CREATE (c)-[:HAS_OCCURRENCE]->(o)
+        RETURN o
+      `;
+
+      const result = await session.run(query, { createOccurrenceDto });
+
+      return result.records.map(record => record.get('o').properties) as Occurrence[];
+    } catch (error) {
+      throw error;
+    } finally {
+      await session.close();
+    }
+  }
+}
