730a77054589f63d26edef46b833f75c0e1826d8
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-03 19:25:26 -0400

    refactor: removed unnecessary interfaces related to finding logic

--- src/modules/commitment/commitment.controller.ts
+++ src/modules/commitment/commitment.controller.ts
@@ -4,11 +4,8 @@ import { CommitmentOccurrenceService } from '../../core/commitment-occurrence/co
 
 import { CreateCommitmentDto } from '../../common/dto/create-commitment.dto';
 import { CreateCommitmentResponse } from './interfaces/create-commitment-response.interface';
-import { FindCommitmentsByPeriodDto } from '../../common/dto/find-commitments-by-period.dto';
-import { FindCommitmentsByPeriodResponse } from './interfaces/find-commitments-by-period-response.interface';
 
 import { CommitmentExceptionFilter } from './commitment.exception.filter';
-import { FindCommitmentsByDateRangeDto } from 'src/common/dto/find-commitments-by-date-range.dto';
 
 @Controller('commitments')
 export class CommitmentController {
