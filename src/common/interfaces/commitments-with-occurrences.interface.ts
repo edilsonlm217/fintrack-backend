0a99a11572e763feebb264b201132216650e4efa
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-03 20:15:06 -0400

    refactor: deleted finding logic interface

--- src/common/interfaces/commitments-with-occurrences.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Commitment } from "./commitment.interface";
-import { Occurrence } from "./occurrence.interface";
-
-export type CommitmentWithOccurrences = Omit<Commitment, 'amount' | 'due_date' | 'start_date' | 'end_date' | 'installments' | 'current_installment'> & {
-  occurrences: Array<Omit<Occurrence, 'commitment_id'>>;
-};
