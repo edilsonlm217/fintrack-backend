1b297475dcf1868433d925734250859f93d4f075
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-03 20:25:13 -0400

    refactor: added identifier to occurrence entity

--- src/common/factories/create-occurrence-dto.factory.ts
+++ src/common/factories/create-occurrence-dto.factory.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';  // Importando a função para gerar UUID
 import { Commitment } from 'src/common/interfaces/commitment.interface';
 
 import { CreateOccurrenceDto } from 'src/common/dto/create-occurrence.dto';
@@ -6,6 +7,7 @@ import { OccurrenceStatus } from 'src/common/enums/occurrence-status.enum';
 export class CreateOccurrenceDtoFactory {
   static createOccurrence(commitment: Commitment, occurrenceDate: string, amount: number): CreateOccurrenceDto {
     return {
+      id: uuidv4(),
       commitment_id: commitment.id,
       due_date: occurrenceDate,
       amount,
