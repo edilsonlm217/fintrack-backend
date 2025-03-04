e696d8aafae5066333976eb437a79a13c7c6972d
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-03 20:25:20 -0400

    refactor: added identifier to occurrence entity

--- src/common/dto/create-occurrence.dto.ts
+++ src/common/dto/create-occurrence.dto.ts
@@ -1,42 +1,39 @@
-import { IsNotEmpty, IsString, IsNumber, IsEnum, IsUUID } from 'class-validator';
 import { OccurrenceStatus } from '../enums/occurrence-status.enum';
 
 /**
  * Data Transfer Object for creating an occurrence.
  * 
- * This class is used to validate the data when creating a new occurrence in the system.
- * It ensures that all necessary fields are provided and are of the correct type.
+ * This class is used to represent the data when creating a new occurrence in the system.
+ * It holds all the necessary fields required to describe the occurrence.
  */
 export class CreateOccurrenceDto {
   /**
-   * ID of the associated commitment.
-   * This field must be a non-empty string.
+   * ID of the occurrence.
+   * This field is a valid UUID.
    */
-  @IsNotEmpty()
-  @IsString()
+  id: string;
+
+  /**
+   * ID of the associated commitment.
+   * This field is a non-empty string.
+   */
   commitment_id: string;
 
   /**
    * Due date of the occurrence in the format YYYY-MM-DD.
-   * This field must be a non-empty string.
+   * This field is a string.
    */
-  @IsNotEmpty()
-  @IsString()
   due_date: string;
 
   /**
    * Amount of the occurrence.
-   * This field must be a non-empty number.
+   * This field is a number.
    */
-  @IsNotEmpty()
-  @IsNumber()
   amount: number;
 
   /**
    * Status of the occurrence.
-   * This field must be a non-empty value from the OccurrenceStatus enum.
+   * This field must be a valid value from the OccurrenceStatus enum.
    */
-  @IsNotEmpty()
-  @IsEnum(OccurrenceStatus) // Use the OccurrenceStatus enum
   status: OccurrenceStatus;
 }
