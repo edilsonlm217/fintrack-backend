e360bd70240a73f8457bf23d5106650f82ff5715
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-04 15:30:21 -0400

    chore: improved documentation

--- src/common/enums/commitment-type.enum.ts
+++ src/common/enums/commitment-type.enum.ts
@@ -1,5 +1,19 @@
+/**
+ * Enum representing different types of financial commitments.
+ */
 export enum CommitmentType {
+  /**
+   * A recurring commitment, such as subscriptions or monthly bills.
+   */
   RECURRING = 'recurring',
+
+  /**
+   * An installment commitment, such as purchases made with multiple credit card payments.
+   */
   INSTALLMENT = 'installment',
+
+  /**
+   * A one-time commitment, such as a single purchase or a one-time payment.
+   */
   ONE_TIME = 'one_time',
 }
